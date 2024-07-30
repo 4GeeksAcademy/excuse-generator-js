c588046ebc12b024b4e164ec7bd38ea2e89c73a9
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-07-30 19:57:47 +0000

    proyect finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let first = Math.floor(Math.random() * who.length);
+  let second = Math.floor(Math.random() * action.length);
+  let third = Math.floor(Math.random() * what.length);
+  let fourth = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[first] + " " + action[second] + " " + what[third] + " " + when[fourth];
 };
